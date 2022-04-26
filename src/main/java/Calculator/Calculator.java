37ecc177b653db85131538eb1de05662c35b2a31
Nastevych <n.ilya93@gmail.com>
2022-04-26 11:16:53 +0300

    Commit 4

--- src/main/java/Calculator/Calculator.java
+++ src/main/java/Calculator/Calculator.java
@@ -1,89 +1,78 @@
 package Calculator;
 import java.text.DecimalFormat;
-import java.util.Scanner;
+import java.util.List;
+
 public class Calculator {
-    private String operation;
-    private Scanner scanner;
+    private static final List<String> SUPPORTED_OPERATIONS = List.of("+", "/", "-", "*", "^");
 
-    public Calculator() {
-        this.scanner = new Scanner(System.in);
-    }
-
-    public String calculate() {
+    public String calculate(double firstNumber, double secondNumber, String operation) {
         System.out.println("Simple Calculator :)");
-        System.out.println("How it works: number + <Enter> + number + <Enter> + operation.");
-        while (!"q".equals(operation)) {
+        while (SUPPORTED_OPERATIONS.contains(operation)) {
             try {
-                double result = calculationStep();
+                double result = calculationStep(firstNumber, secondNumber, operation);
                 return new DecimalFormat("#,##0.00").format(result);
             } catch (NumberFormatException er) {
                 System.err.println("Wrong input data! You can use only numbers.");
-            } catch (UnsupportedOperationException ex) {
-                System.err.println("The operation is not supported! Only '+, -, *, /, ^' is available.");
-            } catch (ArithmeticException et){
+            } catch (ArithmeticException et) {
                 System.err.println("You can't divide by zero.");
             }
         }
-    }
-
-    private double calculationStep () throws NumberFormatException {
-
-        System.out.println("Enter first number:");
-        double firstNumber = Double.parseDouble(scanner.nextLine());
-
-        System.out.println("Enter second number:");
-        double secondNumber = Double.parseDouble(scanner.nextLine());
-
-        System.out.println("Enter operation to perform or q for Quit. (only '+, -, *, /, ^' is available):");
-        operation = scanner.nextLine();
-
-        return performOperation(firstNumber, secondNumber);
+        throw new IllegalStateException(String.format("Operation '%s' is not supported", operation));
     }
 
 
-    private double performOperation (double firstNumber, double secondNumber) {
-        double result;
-        switch (operation){
-            case "+":
-                result = plus(firstNumber, secondNumber);
-                break;
-            case "-":
-                result = minus(firstNumber, secondNumber);
-                break;
-            case "*":
-                result = multiply(firstNumber, secondNumber);
-                break;
-            case "/":
-                if (secondNumber == 0){throw new ArithmeticException(String.format("Can't divide by zero", operation));
-                } else
-                    result = divide(firstNumber, secondNumber);
-                break;
-            case "^":
-                result = pow(firstNumber, secondNumber);
-                break;
-            case "q":
-                System.out.println("You quit! Bye ;)");
-                System.exit(0);
-            default:
-                throw new UnsupportedOperationException(String.format("The operation '%s' is not supported", operation));
+        public double calculationStep (double firstNumber, double secondNumber, String operation){
+
+            return performOperation(firstNumber, secondNumber, operation);
         }
-        System.out.println("Good job! Result for " + firstNumber + " " + operation + " " + secondNumber + " is");
-        return  result;
-    }
 
-    private double plus(double firstNumber, double secondNumber){
-        return firstNumber + secondNumber;
-    }
-    private  double minus(double firstNumber, double secondNumber){
-        return  firstNumber - secondNumber;
-    }
-    private  double multiply(double firstNumber, double secondNumber){
-        return  firstNumber * secondNumber;
-    }
-    private  double divide(double firstNumber, double secondNumber){
-        return  firstNumber / secondNumber;
-    }
-    private double pow (double firstNumber, double secondNumber){
-        return Math.pow(firstNumber,secondNumber);
-    }
+
+        private double performOperation (double firstNumber, double secondNumber, String operation) throws ArithmeticException {
+            double result;
+            switch (operation) {
+                case "+":
+                    result = plus(firstNumber, secondNumber);
+                    break;
+                case "-":
+                    result = minus(firstNumber, secondNumber);
+                    break;
+                case "*":
+                    result = multiply(firstNumber, secondNumber);
+                    break;
+                case "/":
+                    if (secondNumber == 0) {
+                        throw new ArithmeticException(String.format("Can't divide by zero", operation));
+                    } else
+                        result = divide(firstNumber, secondNumber);
+                    break;
+                case "^":
+                    result = pow(firstNumber, secondNumber);
+                    break;
+                default:
+                    result = 0;
+                    break;
+            }
+            System.out.println("Good job! Result for " + firstNumber + " " + operation + " " + secondNumber + " is " + result);
+            return result;
+        }
+
+        private double plus ( double firstNumber, double secondNumber){
+            return firstNumber + secondNumber;
+        }
+        private double minus ( double firstNumber, double secondNumber){
+            return firstNumber - secondNumber;
+        }
+        private double multiply ( double firstNumber, double secondNumber){
+            return firstNumber * secondNumber;
+        }
+        private double divide ( double firstNumber, double secondNumber){
+            return firstNumber / secondNumber;
+        }
+        private double pow ( double firstNumber, double secondNumber){
+            return Math.pow(firstNumber, secondNumber);
+        }
 }
+
+
+
+
--- src/test/java/Calculator/TestCalculator.java
+++ src/test/java/Calculator/TestCalculator.java
@@ -8,16 +8,8 @@ import java.io.InputStream;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.*;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvFileSource;
@@ -28,22 +20,50 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class TestCalculator {
     private Calculator calculator;
 
-    @Test
-    @DisplayName("Test of sum")
-    void testSum(){
-        //GIVEN
-        ByteArrayInputStream first = new ByteArrayInputStream("1".getBytes());
-        System.setIn(first);
-        ByteArrayInputStream second = new ByteArrayInputStream("1".getBytes());
-        System.setIn(second);
-        ByteArrayInputStream operation = new ByteArrayInputStream("+".getBytes());
-        System.setIn(operation);
-        String expectedResult = "2";
-        //WHEN
-        String actualResult = calculator.calculate();
+    @BeforeAll
+    static void initBeforeAll() {
+        System.out.println("Start");
+    }
 
+    @AfterAll
+    static void initAfterAll() {
+        System.out.println("Finish");
+    }
+
+    @BeforeEach
+    void init() {
+        calculator = new Calculator();
+    }
+
+    @ParameterizedTest
+    @DisplayName("Test of operations")
+    @MethodSource("provideValidData")
+    void testOperations (double firstNumber, double secondNumber, String operation,  String expectedResult) {
+        //WHEN
+        String actualResult = calculator.calculate(firstNumber, secondNumber, operation);
         //THEN
         assertEquals(expectedResult, actualResult);
     }
+
+    private static Stream<Arguments> provideValidData() {
+        return Stream.of(
+                Arguments.of(1, 2, "+", "3,00"),
+                Arguments.of(2,2,"/","1,00"),
+                Arguments.of(3,2,"-", "1,00"),
+                Arguments.of(5,5,"*","25,00"),
+                Arguments.of(4,4,"^","256,00")
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("Test of operations in Csv")
+    @CsvFileSource (resources = "/testCalculatorData.csv", delimiter = ':')
+    void testOperationsCsv(double firstNumber, double secondNumber, String operation,  String expectedResult){
+        //WHEN
+        String actualResult = calculator.calculate(firstNumber, secondNumber, operation);
+        //THEN
+        assertEquals(expectedResult, actualResult);
+    }
+
 
 }
